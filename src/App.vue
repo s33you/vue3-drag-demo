f49215137fec424ec336016f2fa51414a63438f8
tanwenbin <tanwenbin@luotuoma.com>
2021-07-05 15:05:20 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<jsx/>
+
+</template>
+
+<script lang="ts">
+import { defineComponent, render } from 'vue'
+import jsx from '@/components/Jsx.vue'
+export default defineComponent({
+  name: 'App',
+  components: {
+    jsx
+  },
+ 
+})
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Jsx.vue
@@ -0,0 +1,13 @@
+
+<script lang="tsx">
+import { defineComponent} from "vue"
+export default defineComponent({
+    setup(){
+
+    },
+    render(){
+        return <div>1sadas122112</div>
+    }
+});
+</script>
+
