fafcad1244d613850e1cdb6fcb7c5ea558c2b1de
tanwenbin <tanwenbin@luotuoma.com>
2021-07-05 17:53:12 +0800

    feat: add element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
 <jsx/>
-
+<el-button type="primary">11</el-button>
 </template>
 
 <script lang="ts">
 import { defineComponent, render } from 'vue'
+import {ElButton} from 'element-plus'
 import jsx from '@/components/Jsx.vue'
 export default defineComponent({
   name: 'App',
   components: {
-    jsx
+    jsx,
+    ElButton
   },
  
 })
